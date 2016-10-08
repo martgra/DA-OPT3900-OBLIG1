fd4920c2c85eeb50f8e61e20cc2b40614e70d1a0
JasonBourne <JasonBourne@DESKTOP-B3T9JJV>
2016-10-08 12:28:19 +0200

    kommentarer på klassene calculatecost, generategraph, greedy itterative og halve greedy method.

--- ObligEn/ObligEn/GreedyItterative.cs
+++ ObligEn/ObligEn/GreedyItterative.cs
@@ -11,44 +11,60 @@ namespace ObligEn
         internal static object Text;
 
         public static int [] greedyItterative(int [] route, int[,] cities )
+        // metode for å regne ut korteste rute gjennom en greedy itterativ måte. Initieres ved å gi array som inneholder rute, samt graf som beskriver distanser mellom byene
         {
 
             int[] routeCopy = new int[route.GetLength(0)];
+            // oppretter tabell som ruten skal kopieres over i. Opprettet pga problemer med referanse til objektet
+            int itteration = 100;
+            //antall ganger metoden skal forsøke å finne bedre rute.
             int length = route.GetLength(0);
+            // får lengde på ruten
             Array.Copy(route, routeCopy, length);
-
+            //kopierer ruten til kopitabellen routeCopy
             int bestResult = CalculateCost.calculateTotalCost(routeCopy, cities);
+            //oppretter og initerer "best result" variabel. skal brukes for å avgjøre om en rute skal forkastes eller beholdes
             int currentRoute = CalculateCost.calculateTotalCost(routeCopy, cities);
-            
-
+            // oppretter og initerer variabel som inneholder verdi på nåværende rute
             Random rnd = new Random();
+            // oppretter objekt av typen Random.
 
-            for (int i = 0; i < 100; i ++)
+            for (int i = 0; i < itteration; i ++)
+            // for-løkke som kjører gitt antall ganger. ingen direkte stoppkriterium
             {
                 int cityOne = rnd.Next(0, length);
+                // oppretter lokal variabel for tilfeldig posisjon 1 i ruten. Gis tilfelldig verdi i intervall antall byer.
                 int cityTwo = rnd.Next(0, length);
+                // oppretter lokal variabel for tilfeldig posisjon 2 i ruten. Gis tilfelldig verdi i intervall antall byer.
 
-                if(cityOne != cityTwo)
+                if (cityOne != cityTwo)
+                //sjekker at by nr 1 ikke er lagret i samme posisjon som by nr 2.
                 {
                     int temp1 = routeCopy[cityOne];
+                    // mellomlagerer hvilken by som befinner seg i den første posisjonen i ruten. 
                     int temp2 = routeCopy[cityTwo];
+                    // mellomlagrer hvilken by som befinner seg på den andre posisjonen i ruten.
 
                     routeCopy[cityOne] = temp2;
+                    // legger byen som ligger lagret i posisjon 2 i posisjon 1.
                     routeCopy[cityTwo] = temp1;
+                    // legger by som ligger lagret i posisjon 1 i posisjon 2. 
 
                     currentRoute = CalculateCost.calculateTotalCost(routeCopy, cities);
+                    // kalkulerer verdien på ruten etter bytte av rekkefølge.
 
                     if(currentRoute < bestResult)
                     {
                         bestResult = currentRoute;
                     }
+                    // hvis det nåværende resultatet er bedre enn beste resultat så oppdateres beste resultat til nåværende resultat og ruten beholdes.
 
                     else
                     {
                         routeCopy[cityOne] = temp1;
                         routeCopy[cityTwo] = temp2;
                     }
-                    
+                    // hvis nåværende rute ikke er bedre enn beste rute revereseres endringen.                     
 
                 }
 
@@ -56,6 +72,7 @@ namespace ObligEn
             }
             
             return routeCopy;
+            // returnerer den forbedrede ruten.
         }        
     }
 }
--- ObligEn/ObligEn/GreedyMethod.cs
+++ ObligEn/ObligEn/GreedyMethod.cs
@@ -7,32 +7,46 @@ using System.Threading.Tasks;
 namespace ObligEn
 {
     public class GreedyMethod
+    // klasse som inneholder metode for å kalkulere beste rute basert på greedy metode.
     {
         public static int[] greedyRoute(int[,] cities)
+        // metode som regner ut beste rute basert på greedy metode. initialiseres ved å sende inn array cities.
         {
             int[] route = new int[cities.GetLength(0)];
+            // initierer array route av typen int med lengden til cities i dimensjon 0. Brukes til å lagre ruten på byene.
             bool[] connected = new bool[cities.GetLength(0)];
+            // initierer array connected av typen boolean. Gis lengden på lengden på cities i dimensjon 0. 
             int i, j;
+            // oppretter variablene i og j.
             Random rnd = new Random();
-
+            // oppretter objektet rnd av typen Random.
 
             for (i = 0; i < cities.GetLength(0); i++)
+            /* forløkke som går lik lengden cities i dimensjon 1. Fyller inn array route med verdien til i 
+                fyller inn verdien til connected i posisjon i med false. 
+             */
             {
                 route[i] = i;
                 connected[i] = false;
             }
-
+            
             i = route[rnd.Next(0, cities.GetLength(0))];
+            // veldger tilfeldig verdi for i innenfor intervallet 0 og lengden på cities i dimensjon 0. 
             route[0] = i;
+            // legger den tilfeldige byen i starten av ruten
             route[i] = 0;
-
-
-            //connected[0] = true;
+            // legger verdien som ligger i starten av ruten til posisjon til verdien i hadde. 
+            connected[0] = true;
+            // angir at den første i byen i ruten er valgt.
             int current = route[0];
+            // gir variablen current verdien til byen som ligger lagret i første posisjon i ruten.
             int min;
+            // oppretter verdien min av typen int. skal brukes til å finne den billigste veien til neste by.
             int next = 1;
+            // oppretter variabel for å kunne lagre neste by i ruten. 
 
             for (i = 0; i < cities.GetLength(0) - 1; i++)
+            // løkken kjører for å finne billigste verdi til neste by fra nåværende posisjon.
             {
                 min = -1;
 
